66a545a4b6d7518d1831f8a0f1eb7548068fb8a0
Yousef Rastegari <yousef@getlocalmeasure.com>
2024-06-15 14:28:50 +1000

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -23,6 +25,8 @@ function App() {
   }
 
   return (
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -38,7 +42,10 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+      )}
+    </Authenticator>
   );
 }
 
